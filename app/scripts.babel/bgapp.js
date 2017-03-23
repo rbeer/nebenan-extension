f8e1ca7a1474188f67816c9f275747551c2dc9ba
Raphael Beer <raphael.beer@protonmail.com>
2017-03-23 02:14:15 +0100

    Add: Set browserAction icon when status updates

--- app/scripts.babel/bgapp.js
+++ app/scripts.babel/bgapp.js
@@ -41,17 +41,49 @@ define(['apiclient'], (APIClient) => {
     });
   };
 
+  /**
+   * Updates local stats counters
+   * @return {Promise}
+   */
+  app.updateStats = () => {
+    return new Promise((resolve, reject) => {
+      app.getToken().then(app.api.getCounterStats).then((stats) => {
+        try {
+          let jstats = JSON.parse(stats);
+          devlog(jstats);
+          resolve(jstats);
+        } catch (err) {
+          reject(err);
+        }
+      });
+    });
+  };
+
+  /**
+   * Update browserAction icon and badge
+   * @param  {object} stats counter_stats.json, received from API
+   * @param  {number} stats.hood_active_users_count
+   * @param  {number} stats.new_messages_count
+   * @param  {number} stats.new_notifications_count
+   */
+  app.updateBrowserAction = (stats) => {
+    console.log('Updating browser action with:', stats);
+
+    let newMessages = stats.new_messages_count;
+    let newNotifications = stats.new_notifications_count;
+    let allNew = newMessages + newNotifications;
+
+    let iconPath = `images/icon-${allNew > 0 ? 'unread' : 'read'}_16.png`;
+    chrome.browserAction.setIcon({ path: iconPath });
+  };
+
   chrome.runtime.onInstalled.addListener(details => {
     devlog('onInstalled:', details);
-    chrome.alarms.create('nebenan', { when: Date.now(), periodInMinutes: 1 });
 
+    chrome.alarms.create('nebenan', { when: Date.now(), periodInMinutes: 1 });
     chrome.alarms.onAlarm.addListener((alarm) => {
       devlog('Alaram:', alarm);
-      app.getToken().then(app.api.getCounterStats).then((stats) => {
-        let jstats = JSON.parse(stats);
-        devlog(jstats);
-      })
-      .catch((err) => console.error(err));
+      app.updateStats().then(app.updateBrowserAction);
     });
 
   });
