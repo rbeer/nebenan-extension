151d56932a6a3e588dc1686804f52e1cf81d52aa
Raphael Beer <raphael.beer@protonmail.com>
2017-04-04 15:18:26 +0200

    Add: Requesting notifications(.json) from API
      - APIClient.getNotifications
      - jsdoc type definitions
        - APIClient.NMessage - subset of API message
          object, linked to notification
        - APIClient.NItem - subset of API
          notification object
        - APIClient.NType - enum for
          NItem.notification_type_id

--- app/scripts.babel/bg/apiclient.js
+++ app/scripts.babel/bg/apiclient.js
@@ -36,7 +36,7 @@ define([ 'bg/cookies' ], (Cookies) => {
      * @static
      * @param  {!APIClient.XHROptions} options - Options passed to XMLHttpRequest
      * @throws {TypeError} if (!options)
-     * @return {Promise}
+     * @return {Promise}                       - Resolves with response body
      */
     static callAPI(options) {
 
@@ -80,7 +80,7 @@ define([ 'bg/cookies' ], (Cookies) => {
      * @see APIClient.callAPI
      * @memberOf APIClient
      * @static
-     * @return {Promise}
+     * @return {Promise} - Resolves with response body from APIClient.callAPI
      */
     static getCounterStats(cached) {
       if (cached) {
@@ -95,7 +95,70 @@ define([ 'bg/cookies' ], (Cookies) => {
       })
       .then(APIClient.callAPI);
     }
+
+    /**
+     * Requests notifications.json
+     * @see APIClient.callAPI
+     * @memberOf APIClient
+     * @static
+     * @return {Promise} - Resolves with response body from APIClient.callAPI
+     */
+    static getNotifications() {
+      return Cookies.getToken()
+      .then((token) => {
+        let xhrOptions = APIClient.XHR_DEFAULTS;
+        xhrOptions.url += '/notifications.json?per_page=20';
+        xhrOptions.token = token;
+        return xhrOptions;
+      })
+      .then(APIClient.callAPI);
+    }
   };
+
+  /**
+   * Message, linked to an NItem
+   * @typedef {Object} NMessage
+   * @property {Number}   id                        - Message's id
+   * @property {Number}   created                   - UNIX epoch timestamp, millisecond precision
+   * @property {Number}   user_id                   - Author's id
+   * @property {Number}   hood_message_type_id      - ! UNKNOWN !
+   * @property {Number}   hood_message_category_id  - ! UNKNOWN !
+   * @property {?Number}  hood_group_id             - Id of group, message was posted in
+   * @property {String}   body                      - Message body
+   * @property {Number}   hood_id                   - Id of author's hood
+   * @property {String}   subject                   - Message's title
+   * @property {Bool}     house_group               - Whether message's author lives in the same house
+   * @property {Object[]} images                    - Images, embedded in post
+   * @property {Number}   images.id                 - Image's id
+   * @property {String}   images.url                - Image's url
+   * @property {String}   images.url_medium         - Image's url (medium size/thumb)
+   * @property {Object}   user                      - Message's author
+   * @property {Number}   user.id                   - Author's id
+   * @property {String}   user.firstname            - Author's first name
+   * @property {String}   user.lastname             - Author's last name (might be shortened to (\w\.) )
+   * @property {String}   user.photo_thumb_url      - Auhtor's profile image (thumbnail size)
+   * @property {Number}   user.hood_id              - ID of author's hood
+   * @property {String}   user.hood_title           - Name of author's hood
+   * @memberOf APIClient
+   */
+
+  /**
+   * Notification
+   * @typedef {Object} NItem
+   * @property {Number}   id
+   * @property {NMessage} hood_message          - Message that triggered the notification
+   * @property {Number}   created_at_timestamp  - UNIX epoch timestamp, millisecond precision
+   * @property {NType}    notification_type_id  - Type of notification (market, feed, group, message)
+   * @memberOf APIClient
+   */
+
+  /**
+   * Notification Type, number values from API
+   * @typedef {Object} NType
+   * @property {Number} 400  - Marketplace notification
+   * @property {Number} 1200 - Feed notification
+   * @memberOf APIClient
+   */
 
   return APIClient;
 
