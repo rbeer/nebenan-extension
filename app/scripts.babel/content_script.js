75430361b328f8e9453f3a83251b1444c5f76cfa
Raphael Beer <raphael.beer@protonmail.com>
2017-04-20 01:07:47 +0200

    Rework: Ok, let's try this again. ^_^

--- app/scripts.babel/content_script.js
+++ app/scripts.babel/content_script.js
@@ -1,35 +1,69 @@
-'use strict';
-
 (() => {
+  'use strict';
 
-  const targetClasses = {
-    LIST: 'c-feed_list-content',
-    MARKET: '.c-feed_marketplace',
-    MARKET_PREVIEW: 'article.c-feed_marketplace_preview'
+  /**
+   * Selectors for querySelector()
+   * @type {Object.<String, String>}
+   */
+  const selectors = {
+    // root Node for feed list
+    LIST: 'section.c-feed_list',
+    // actual feed list Node
+    LISTITEM: 'ul.c-feed_list-item',
+    // Nodes to filter / hide
+    UNWANTED: [ '.c-feed_marketplace', 'article.c-feed_marketplace_preview' ]
   };
 
-  let filterEntries = (mutations) => {
-    mutations.forEach((mRecord) => {
+  /**
+   * Filters unwanted entries when <section.c-feed_list> has been populated, initially
+   *   - As opposed to e.g. infinite scroll adding new items
+   * @param  {Array.<MutationRecord>} mRecords - MutationRecords caught by initObserver
+   */
+  let filterOnInitObservation = (mRecords) => {
 
-      if (mRecord.target.className === targetClasses.LIST) {
-        console.debug('found list:', mRecord.target);
+    // <section.c-feed_list> receives <ul.c-feed_list-content> Element, only
+    let list = mRecords[0].addedNodes.item(0);
+    console.debug('.c-feed_list-content:', list);
 
-        // remove marketplace entries (will most likely produce flicker)
-        Array.from(mRecord.target.children).forEach((li) => {
-          if (li.querySelector(targetClasses.MARKET) ||
-              li.querySelector(targetClasses.MARKET_PREVIEW)) {
-            console.debug('hiding:', li);
-            li.setAttribute('hidden', '');
-          }
-        });
-      }
-    });
+    // hide unwanted entries from initial state
+    let liNodes = Array.from(list.querySelectorAll(selectors.UNWANTED + '')).map(findParentLI);
+    hideEntries(liNodes);
+
+    // stop observing root Node <section.c-feed_list>
+    initObserver.disconnect();
   };
 
-  // react seems to create a #document and dump it
-  // as one into the DOM; so the observation needs to apply to the
-  // <body>, as the target <ul> isn't available
-  let ulObserver = new MutationObserver(filterEntries);
-  ulObserver.observe(document.body, { childList: true, subtree: true });
+  /**
+   * Finds parent list item (<li>) to matched entries
+   *   - think JQuery's .parent() !
+   * @param  {Node} node - Child Node to start searching upwards from
+   * @return {Node}
+   */
+  let findParentLI = (node) => {
+    let parent = node.parentNode;
+    let liClass = selectors.LISTITEM.slice(3);
+
+    while (parent.className !== liClass) {
+      parent = parent.parentNode;
+    }
+
+    return parent;
+  };
+
+  /**
+   * Adds 'hidden' attribute to every Node in an Array
+   *   - using site's CSS rule [hidden] { display:none }
+   * @param  {Array.<Node>} liNodes - Nodes to hide
+   */
+  let hideEntries = (liNodes) => {
+    liNodes.forEach((node) => node.setAttribute('hidden', ''));
+  };
+
+  // react creates the entire <ul.c-feed_list-content> (with its <li> children)
+  // and dumps it as one into the DOM; target Element is <section.c-feed_list>,
+  // so let that be our observation target, as well. :)
+  let initTarget = document.querySelector(selectors.LIST);
+  let initObserver = new MutationObserver(filterOnInitObservation);
+  initObserver.observe(initTarget, { childList: true });
 
 })();
