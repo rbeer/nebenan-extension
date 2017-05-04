e76308917b2eefc4e1362b6223b8b030e237dd91
Raphael Beer <raphael.beer@protonmail.com>
2017-05-04 09:56:52 +0200

    Chore: docs

--- app/scripts.babel/bg/apiclient/nstats.js
+++ app/scripts.babel/bg/apiclient/nstats.js
@@ -1,6 +1,18 @@
 define(['bg/apiclient/nsubset'], (NSubset) => {
-
+  /**
+   * @class  Status Data
+   * @memberOf APIClient
+   * @extends {APIClient.NSubset}
+   */
   class NStats extends NSubset {
+    /**
+     * Takes a notifcation_stats object form the API and creates a subset
+     * with members of interest. Only those subset members are documented.
+     * @param  {Object} raw - response Object from notification_stats.json
+     * @param  {Number} raw.new_messages_count      - \# of new private conversation events
+     * @param  {Number} raw.new_notifications_count - \# of new general notifications
+     * @return {NStats}
+     */
     constructor(raw) {
       let compatibility_hack = function() {
         this.messages = 'new_messages_count';
